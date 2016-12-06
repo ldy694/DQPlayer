6d9d4c8b073de07bc4d7e7553a3c23f577e8a5da
linxingdong <linxingdong@foxmail.com>
2016-12-06 14:33:52 +0800

    .gitignore配置

---
+++ DQPlayer/DQPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DQPlayer
+//
+//  Created by 林兴栋 on 2016/12/6.
+//  Copyright © 2016年 林兴栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DQPlayer/DQPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DQPlayer
+//
+//  Created by 林兴栋 on 2016/12/6.
+//  Copyright © 2016年 林兴栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
